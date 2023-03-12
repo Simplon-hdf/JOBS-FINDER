4fe1f5b12480b7367ea2ab3e97c2e7402a191e5f
Salim <salim.hassan.welleat@gmail.com>
2023-03-12 21:01:17 +0100

    Add: server connected to BD correctelly

---
+++ jobs-finder-backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const pg = require("pg");
+
+const pool = new pg.Pool({
+  host: "localhost",
+  port: 5432,
+  database: "postgres",
+  user: "salimhassanmohamed" /* user name and the password we provided to the DB(the same as connecting to DB with migrate)  */,
+  pasword: "",
+});
+
+//To check the connection is established
+pool.query("SELECT   1 + 1;").then((res) => console.log(res));
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
