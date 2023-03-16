bdaabb7da007bab753fcdda369284845d8dd3f75
Salim <salim.hassan.welleat@gmail.com>
2023-03-16 09:31:39 +0100

    Add: ports to postgres

--- jobs-finder-backend/server.js
+++ jobs-finder-backend/server.js
@@ -5,8 +5,8 @@ const pool = new pg.Pool({
   host: "localhost",
   port: 5432,
   database: "postgres",
-  user: "salimhassanmohamed" /* user name and the password we provided to the DB(the same as connecting to DB with migrate)  */,
-  pasword: "",
+  user: "postgres" /* user name and the password we provided to the DB(the same as connecting to DB with migrate)  */,
+  pasword: "postgres",
 });
 
 //To check the connection is established
