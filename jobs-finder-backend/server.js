12fedd873fba5215d8093ee34b71d91ccb4ceacd
Salim <salim.hassan.welleat@gmail.com>
2023-03-24 12:33:43 +0100

    Add: migrate up

--- jobs-finder-backend/server.js
+++ jobs-finder-backend/server.js
@@ -13,7 +13,7 @@ const pool = new pg.Pool(poolConfig);
 
 //To check the connection is established
 pool.query("SELECT   1 + 1;").then((res) => {
-  console.log("Coonected avec seccess");
+  console.log("Connected avec success");
   console.log("====================================");
   console.log(res);
   console.log("====================================");
@@ -24,6 +24,16 @@ const port = process.env.PORT || 3002;
 app.get("/", (req, res) => {
   console.log("Hello world");
   res.send("Hello world");
+});
+app.get("/comment", async (req, res) => {
+  try {
+    await pool.query(
+      "CREATE TABLE schools( id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100) )"
+    );
+    res.send("DB Created");
+  } catch (error) {
+    res.status(500).send("Error occurs");
+  }
 });
 app.use(express.urlencoded({ extended: true }));
 
